512492587ccd9cd5ab26a4dee707fb6b2e0ebf36
casserekevin <kevin_cassere13@hotmail.com>
2020-09-21 19:58:18 -0300

    Aula 5 - Actions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const BUY_CAKE = 'BUY_CAKE'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'First Redux Action'
+    }
+}
