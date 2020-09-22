353e1592f2bd9bbe61622cc9e3e775e282cc40d2
casserekevin <kevin_cassere13@hotmail.com>
2020-09-22 01:00:00 -0300

    Aula 10 - Combine Reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const redux = require('redux')
 
 //Action
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake(){
     return {
@@ -11,15 +12,31 @@ function buyCake(){
     }
 }
 
+function buyIceCream(){
+    return {
+        type: BUY_ICECREAM,
+        info: 'First Redux Action'
+    }
+}
+
 
 
 //Reducer
 // reducer(previousState, action) => newState
-const initialState = {
-    numOfCakes: 10
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+const initialCakeState = {
+    numOfCakes: 10,
 }
 
-const reducer = (state = initialState, action) => {
+const InitialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE:
             return {
@@ -32,14 +49,33 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+const iceCreamReducer = (state = InitialIceCreamState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1
+            }    
+
+        default: 
+            return state
+    }
+}
+// reducer has access to the initial state
+const rootReducer = redux.combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+
 
 //Store
-// reducer has access to the initial state
-const store = redux.createStore(reducer)
+const store = redux.createStore(rootReducer)
 console.log('Initial State: ', store.getState())
 // Subscribe a listener to the store
 const unsubscribe = store.subscribe(() => console.log('Updated State: ', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
